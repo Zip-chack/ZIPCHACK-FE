c7cd273b39382fec697069249752cea39a324a11
jiucchu <jiucchu@gmail.com>
2025-12-05 10:44:35 +0900

    chore: 초기 프로젝트 세팅

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id="app" class="min-h-screen flex flex-col">
+    <AppHeader />
+    <main class="flex-1">
+      <router-view />
+    </main>
+    <AppFooter />
+  </div>
+</template>
+
+<script></script>
