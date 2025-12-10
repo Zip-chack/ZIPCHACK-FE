93a6125886b122ebcfd65ed19201a02c1b741cbb
jiucchu <jiucchu@gmail.com>
2025-12-10 16:22:05 +0900

    refactor: 연락하기 컴포넌트 분리

---
+++ src/components/common/ContactCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="card p-6 sticky top-24">
+    <h3 class="font-semibold text-gray-900 mb-4">연락하기</h3>
+    <button class="btn-primary w-full mb-3" @click="handlePhoneCall">
+      전화 문의
+    </button>
+    <button class="btn-secondary w-full" @click="handleMessage">
+      메시지 보내기
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ContactCard",
+  methods: {
+    handlePhoneCall() {
+      // TODO: 전화 문의 기능 구현
+      console.log("전화 문의");
+    },
+    handleMessage() {
+      // TODO: 메시지 보내기 기능 구현
+      console.log("메시지 보내기");
+    },
+  },
+};
+</script>
