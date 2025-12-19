7a7f62331b6947bd4b1c3d2756de44a0568ad1cf
jiucchu <jiucchu@gmail.com>
2025-12-19 16:43:57 +0900

    fix: 내가 등록한 매물의 카드 화면 변경

--- src/components/common/ContactCard.vue
+++ src/components/common/ContactCard.vue
@@ -1,12 +1,27 @@
 <template>
   <div class="card p-6">
-    <h3 class="font-semibold text-gray-900 mb-4">연락하기</h3>
-    <button class="btn-primary w-full mb-3" @click="handlePhoneCall">
-      전화 문의
-    </button>
-    <button v-if="!isOwner" class="btn-secondary w-full" @click="handleMessage">
-      메시지 보내기
-    </button>
+    <h3 v-if="!isOwner" class="font-semibold text-gray-900 mb-4">연락하기</h3>
+    <h3 v-else class="font-semibold text-gray-900 mb-4">매물 관리</h3>
+
+    <!-- 소유자가 아닌 경우: 연락하기 버튼들 -->
+    <template v-if="!isOwner">
+      <button class="btn-primary w-full mb-3" @click="handlePhoneCall">
+        전화 문의
+      </button>
+      <button class="btn-secondary w-full" @click="handleMessage">
+        메시지 보내기
+      </button>
+    </template>
+
+    <!-- 소유자인 경우: 매물 수정 버튼 -->
+    <template v-else>
+      <router-link
+        :to="`/listings/${listingId}/edit`"
+        class="btn-primary w-full block text-center"
+      >
+        매물 수정
+      </router-link>
+    </template>
   </div>
 </template>
 
